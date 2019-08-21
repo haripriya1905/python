2c02002cdd7a99ef8a74a39db1718e2c26006391
haripriya1905 <47343789+haripriya1905@users.noreply.github.com>
2019-08-21 16:01:43 +0530

    Update smallest.py

--- smallest.py
+++ smallest.py
@@ -6,16 +6,10 @@ def Nminelements(list1, N):
            
         for j in range(len(list1)):       
             if list1[j]<min1: 
-                min1 = list1[j]; 
-  
-        # remove minimum element from list so  
-        # that it do not come in calculation again          
+                min1 = list1[j];    
         list1.remove(min1); 
-        final_list.append(min1) 
-           
+        final_list.append(min1)       
     print(final_list) 
-   
-# Driver code 
-list1 = [4, 78, 34, 10, 8, 21, 11, 231]; 
+list1 = [4, 78, 34, 10, 8, 21, 11, 230]; 
 N = 2; 
 Nminelements(list1, N) 
